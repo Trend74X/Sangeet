ac475c6ce4eeb041116231a9b7daa0ca4cfe7aa3
ashok-mi <ashok_mi@yonefu.info>
2026-01-30 17:53:46 +0545

    [Fxied :: Filtereed songs] - When on album/artitst page on double click routes to now playing page

--- lib/src/views/filtered_songs.dart
+++ lib/src/views/filtered_songs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:on_audio_query_forked/on_audio_query.dart';
 import 'package:sangeet/src/controller/audio_controller.dart';
+import 'package:sangeet/src/widgets/bottom_nav.dart';
 
 class FilteredSongs extends StatefulWidget {
   const FilteredSongs({super.key});
@@ -44,64 +45,71 @@ class _FilteredSongsState extends State<FilteredSongs> {
       separatorBuilder: (context, index) => const Divider(),
       physics: const ClampingScrollPhysics(),
       itemBuilder: (context, index) {
-        return ListTile(
-          tileColor: _con.nowPlaying.id == _con.filteredSongs[index].id ? Theme.of(context).primaryColor : Theme.of(context).scaffoldBackgroundColor,
-          leading:  QueryArtworkWidget(
-            controller: _con.audioQuery,
-            id: _con.filteredSongs[index].id,
-            type: ArtworkType.AUDIO,
-            nullArtworkWidget: const Image(
-              image: AssetImage('assets/images/appIcon.png'),
-              width: 40.0,
-              height: 40.0,
-            )
-          ),
-          title: SizedBox(
-            width: MediaQuery.of(context).size.width * 0.75,
-            child: Text(
-              _con.filteredSongs[index].title,
-              overflow: TextOverflow.ellipsis
-            )
-          ),
-          subtitle: Row(
-            children: [
-              Row(
-                children: [
-                  const Icon(
-                    Icons.person,
-                    size: 18
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    child: Text(
-                      ' ${_con.filteredSongs[index].artist}',
-                      overflow: TextOverflow.ellipsis
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.album,
-                    size: 18
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    child: Text(
-                      ' ${_con.filteredSongs[index].album}',
-                      overflow: TextOverflow.ellipsis
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          onTap: () {
+        return InkWell(
+          onDoubleTap: () {
             _con.currentPlayingList = List<SongModel>.from(_con.filteredSongs);
             _con.addToNowPlaying(index);
-            setState(() { });
+            Get.to(() => const BottomNavigation(), preventDuplicates: false);
           },
+          child: ListTile(
+            tileColor: _con.nowPlaying.id == _con.filteredSongs[index].id ? Theme.of(context).primaryColor : Theme.of(context).scaffoldBackgroundColor,
+            leading:  QueryArtworkWidget(
+              controller: _con.audioQuery,
+              id: _con.filteredSongs[index].id,
+              type: ArtworkType.AUDIO,
+              nullArtworkWidget: const Image(
+                image: AssetImage('assets/images/appIcon.png'),
+                width: 40.0,
+                height: 40.0,
+              )
+            ),
+            title: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: Text(
+                _con.filteredSongs[index].title,
+                overflow: TextOverflow.ellipsis
+              )
+            ),
+            subtitle: Row(
+              children: [
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.person,
+                      size: 18
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      child: Text(
+                        ' ${_con.filteredSongs[index].artist}',
+                        overflow: TextOverflow.ellipsis
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.album,
+                      size: 18
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      child: Text(
+                        ' ${_con.filteredSongs[index].album}',
+                        overflow: TextOverflow.ellipsis
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            onTap: () {
+              _con.currentPlayingList = List<SongModel>.from(_con.filteredSongs);
+              _con.addToNowPlaying(index);
+              setState(() { });
+            },
+          ),
         );
       }
     );
